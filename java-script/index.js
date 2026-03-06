f0cd4b2fbcd5bd3871b0f7626f4d66225f432f4b
eyasinarafat3485-gif <eyasinarafat3485@gmail.com>
2026-03-06 21:24:35 +0600

    sign in page done

---
+++ java-script/index.js
@@ -0,0 +1,27 @@
+// console.log('Ooooooo')
+
+document.getElementById('btnSignIn').addEventListener('click', function(){
+    console.log('login done')
+    // 1 get the mobile number
+    const inputUsername= document.getElementById('input-Username');
+    const contactUsername= inputUsername.value;
+    console.log(contactUsername);
+
+    // 2 get the pin
+    const inputPin= document.getElementById('input-pin');
+    const contactPin= inputPin.value;
+    console.log(contactPin);
+    
+    // 3 match pin and mobile number
+    if(contactUsername=='admin' && contactPin=='admin123'){
+        // 3.1 true:::>> alert> homepage
+        alert('Sign In Success')
+        window.location.assign("home.html")
+    }
+    // 3.2 false:::>> alert> return
+    else{
+        alert('Sign In Failed');
+    }
+    return;
+    
+});
